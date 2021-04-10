8cc1dd1c4e3ecec17c5d3d6e68850206445e53e2
Aquaakuma <31891793+Aquaakuma@users.noreply.github.com>
2021-04-10 10:50:24 +0800

    Update wallpaper.js

--- wallpaper.js
+++ wallpaper.js
@@ -1,1 +1,2 @@
-document.querySelector('body').style="background-image:url('https://drive.aqualink.top/d/AquaStock/imgUrl/wallhaven-j8xw9m.jpg');background-size:cover;background-attachment:fixed;"
+document.querySelector('body').style="background-image:url('https://drive.aqualink.top/d/AquaStock/imgUrl/wallhaven-j8xw9m.jpg');background-size:cover;background-attachment:fixed;";
+document.querySelector("#app>div > div > div.content > div.files > div > div > div > div").style="background:transparent;";
